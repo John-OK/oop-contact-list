572047d83655b6bcc0cf6952022f6858bc85baef
JohnOK <john.ok.esp@gmail.com>
2022-06-01 23:50:33 +0200

    Added class with outline of structure.

--- contact_list.py
+++ contact_list.py
@@ -1,1 +1,13 @@
 class ContactList():
+    def __init__(self, name):
+        self.name = name
+        self.contacts = contacts
+
+    def add_contact(add_name, phone):
+        pass
+
+    def remove_contact(remove_name):
+        pass
+
+    def find_shared_contacts(contact_list):
+        pass
